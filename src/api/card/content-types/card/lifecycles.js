6d56efa799eb86f4c39a48c0725ba191a12bbfaf
Orkun Zafer Ozturk <orkunzozturk@gmail.com>
2023-04-05 19:09:59 +0800

    lifecycle done

--- src/api/card/content-types/card/lifecycles.js
+++ src/api/card/content-types/card/lifecycles.js
@@ -1,11 +1,12 @@
 // const { resourceLimits } = require("worker_threads");
 // lifecycles.js
-const { FormData } = require("formdata-node");
-const fetch = require("node-fetch");
+// const { FormData } = require("formdata-node");
+// const fetch = require("node-fetch");
 
 const api_url = "http://127.0.0.1:1337";
 module.exports = {
   async afterUpdate(event) {
+    console.log("running after update");
     let dataSource = event.params.data;
     if (
       dataSource.Paragraph &&
@@ -105,6 +106,102 @@ module.exports = {
 
     }
   },
-};
 
-// ADD same lifecycle hook to newly created content
+  async afterCreate(event) {
+    console.log("after create event: ");
+    let dataSource = event.result;
+    // console.log(dataSource);
+    // console.log(event.params);
+    if (
+      dataSource.Paragraph &&
+      dataSource.Paragraph?.length > 0
+    ) {
+
+      // get the image url associated with post:
+      const imageResourceUrl = dataSource.Image.url
+      console.log("image resource: " + imageResourceUrl);
+      // // convert the relative path as an absolute path to the ImageUrl field of the card content-type:
+      const imageUrl = api_url + imageResourceUrl
+      dataSource.ImageUrl = imageUrl;
+      console.log("Data image source link: " + dataSource.ImageUrl);
+
+      // create a canvas
+      const { loadImage, createCanvas } = require("canvas");
+      const fs = require("fs");
+
+      // Dimensions for the image
+      const width = 378;
+      const height = 662.438;
+
+      // Instantiate the canvas object
+      const canvas = createCanvas(width, height);
+      const ctx = canvas.getContext("2d");
+
+      // Fill the rectangle with white
+      ctx.fillStyle = "#a14124";
+      ctx.fillRect(0, 0, width, height);
+      ctx.fillText("hello world", width, height);
+
+      // load the background image
+      loadImage(dataSource.ImageUrl)
+        .then((image) => {
+          // draw the background image on the canvas
+          ctx.drawImage(image, 0, 0, width, height);
+
+          // add Headline to the image
+          ctx.fillStyle = "white";
+          ctx.font = "bold 48px sans-serif";
+          ctx.textAlign = "center";
+          ctx.fillText(dataSource.Headline, width / 2, height / 3);
+
+          // Add Paragraph to image
+          ctx.fillStyle = "white";
+          ctx.font = "bold 48px sans-serif";
+          ctx.textAlign = "center";
+          ctx.fillText(dataSource.Paragraph, width / 2, height / 2);
+
+          // place the canvas to a png buffer
+          const buffer = canvas.toBuffer("image/png");
+          // save the buffer as a file
+          fs.writeFileSync(`./temp/${dataSource.Headline}.png`, buffer);
+          // upload the created file to strapi
+          const mime = require('mime-types'); //used to detect file's mime type
+          const fileName = `${dataSource.Headline}.png`;
+          const filePath = `./temp/${fileName}`
+          const stats = fs.statSync(filePath)
+          return strapi.plugins.upload.services.upload.upload({
+            populate: '*',
+            data:{}, //mandatory declare the data(can be empty), otherwise it will give you an undefined error.
+            files: {
+              path: filePath,
+              name: fileName,
+              type: mime.lookup(filePath), // mime type of the file
+              size: stats.size,
+            },
+        });
+
+
+
+      })
+      .then((response) => {
+        // console.log("File uploaded succesfully", response);
+        const cardId = dataSource.id
+        const fileId = response[0].id;
+        const fileUrl = response[0].url
+        return strapi.query("api::card.card").update({
+          where: {id:cardId},
+          data:{
+            Poster: fileId,
+            PosterUrl: api_url + fileUrl,
+            ImageUrl: imageUrl
+          }
+        })
+
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    }
+  },
+};
