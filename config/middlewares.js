6b263d8f1020fd0d04c267a676a1307e0821f70f
root <orkunzozturk@gmail.com>
2023-05-12 23:22:35 +0800

    added cors middleware options

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,15 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['https://content.xilinshuyuan.cn', 'http://content.xilinshuyuan.cn'],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },  
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
